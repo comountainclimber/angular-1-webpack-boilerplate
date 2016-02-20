746bc7e7b864ef8ded9174a0dcbc596306788ab8
comountainclimber <mlasky46@gmail.com>
2016-02-20 00:33:53 -0700

    some style changes

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.all('/*', function(req, res, next) {
 });
 
 // Creating Server and Listening for Connections \\
-var port = 80;
+var port = 3000;
 app.listen(port, function(){
   console.log('Server running on port ' + port);
 
