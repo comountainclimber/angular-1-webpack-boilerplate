15e5ee86ebb0cb9715096a0e89ad52424066e746
comountainclimber <mlasky46@gmail.com>
2016-02-25 18:06:16 -0700

    dev and production config set up with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,32 @@
 var webpack = require("webpack");
+var WebpackDevServer = require("webpack-dev-server");
+var path = require('path');
+// var config = require('./webpack.config');
 
-// new webpack.optimize.UglifyJsPlugin({
-//     compress: {
-//         warnings: false
-//     }
-// });
-
-// module.exports = {
-//     entry: "./entry.js",
-//     output: {
-//         path: __dirname + "/public/javascripts",
-//         filename: "build.js"
-//     }
-// };
-
-// new webpack.optimize.UglifyJsPlugin({
-//     compress: {
-//         warnings: false
-//     }
+// new WebpackDevServer(webpack(config), {
+//   publicPath: config.output.publicPath,
+//   hot: true,
+//   headers: { 'Access-Control-Allow-Origin': '*' }
 // })
 
 module.exports = {
-  entry: {
-    "bundle": "./entry.js",
-    "bundle.min": "./entry.js",
-  },
-  devtool: "source-map",
+  entry: [
+    'webpack/hot/dev-server',
+    'webpack-dev-server/client?http://localhost:8080',
+    // "bundle": "./entry.js",
+    // "bundle.min": "./entry.js"
+    path.resolve(__dirname, './entry.js')
+  ],
+  devtool: '#eval-source-map',
   output: {
-    path: __dirname + "/public/javascripts",
-    filename: "build.min.js"
+    publicPath: 'http://localhost:8080/',
+    path: path.resolve(__dirname, 'build'),
+    filename: "build.js"
   },
-  plugins: [
-    new webpack.optimize.UglifyJsPlugin({
-      include: /\.min\.js$/,
-      minimize: true
-    })
-  ]
+  // plugins: [
+  //   new webpack.optimize.UglifyJsPlugin({
+  //     include: /\.min\.js$/,
+  //     minimize: false
+  //   })
+  // ]
 };
