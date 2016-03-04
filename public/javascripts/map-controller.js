54f45529ec9dc68fd245fc36463709518ef734af
comountainclimber <mlasky46@gmail.com>
2016-03-03 18:08:04 -0700

    almost responsive

--- public/javascripts/map-controller.js
+++ public/javascripts/map-controller.js
@@ -1,45 +1,102 @@
 angular.module('freshAir')
   .controller('mapCtrl', ['$scope', '$mdDialog', '$mdMedia', function($scope, $mdDialog, $mdMedia) {
       
-    // $scope.locationClicked = $scope.location
-    // console.log($scope.locationClicked)
-    // $scope.locationClicked = "Crested Butte"
-
     $scope.showCb = function(ev, location) {
-        var useFullScreen = ($mdMedia('sm') || $mdMedia('xs'))  && $scope.customFullscreen;
-      
-        $mdDialog.show({
-          // title: 'blumpkin',
-          controller: 'mapCtrl',
-          templateUrl: '../views/partials/modal/dialog1.tmpl.html',
-          parent: angular.element(document.body),
-          targetEvent: ev,
-          clickOutsideToClose:true,
-          fullscreen: useFullScreen
-        })
-    }
+      var useFullScreen ;
+      if ($mdMedia('xs') === true) {
+        useFullScreen = true;
+      }
+      else {
+        useFullScreen = false;
+      }
+      $mdDialog.show({
+        controller: 'mapCtrl',
+        templateUrl: '../views/partials/modal/crested-butte.tmpl.html',
+        parent: angular.element(document.body),
+        targetEvent: ev,
+        clickOutsideToClose:true,
+        fullscreen: useFullScreen
+      });
+    };
 
-    $scope.showSteamboat = function(ev, location) {
-        var useFullScreen = ($mdMedia('sm') || $mdMedia('xs'))  && $scope.customFullscreen;
-      
-        $mdDialog.show({
-          // title: 'blumpkin',
-          controller: 'mapCtrl',
-          templateUrl: '../views/partials/modal/dialog2.tmpl.html',
-          parent: angular.element(document.body),
-          targetEvent: ev,
-          clickOutsideToClose:true,
-          fullscreen: useFullScreen
-        })
-    }
+    $scope.showSteamboat = function(ev, location) { 
+      var useFullScreen;
+      if ($mdMedia('xs') === true) {
+        useFullScreen = true;
+      }
+      else {
+        useFullScreen = false;
+      }
+      $mdDialog.show({
+        controller: 'mapCtrl',
+        templateUrl: '../views/partials/modal/steamboat.tmpl.html',
+        parent: angular.element(document.body),
+        targetEvent: ev,
+        clickOutsideToClose:true,
+        fullscreen: useFullScreen
+      });
+    };
+
+    $scope.showVail = function(ev, location) {
+      var useFullScreen;
+      if ($mdMedia('xs') === true) {
+        useFullScreen = true;
+      }
+      else {
+        useFullScreen = false;
+      }
+      $mdDialog.show({
+        controller: 'mapCtrl',
+        templateUrl: '../views/partials/modal/vail.tmpl.html',
+        parent: angular.element(document.body),
+        targetEvent: ev,
+        clickOutsideToClose:true,
+        fullscreen: useFullScreen
+      });
+    };
+
+    $scope.showAspen = function(ev, location) {
+      var useFullScreen ;
+      if ($mdMedia('xs') === true) {
+        useFullScreen = true;
+      }
+      else {
+        useFullScreen = false;
+      }
+      $mdDialog.show({
+        controller: 'mapCtrl',
+        templateUrl: '../views/partials/modal/aspen.tmpl.html',
+        parent: angular.element(document.body),
+        targetEvent: ev,
+        clickOutsideToClose:true,
+        fullscreen: useFullScreen
+      });
+    };
+
+    $scope.showTelluride = function(ev, location) {
+      var useFullScreen ;
+      if ($mdMedia('xs') === true) {
+        useFullScreen = true;
+      }
+      else {
+        useFullScreen = false;
+      }
+      $mdDialog.show({
+        controller: 'mapCtrl',
+        templateUrl: '../views/partials/modal/telluride.tmpl.html',
+        parent: angular.element(document.body),
+        targetEvent: ev,
+        clickOutsideToClose:true,
+        fullscreen: useFullScreen
+      });
+    };
 
     $scope.hide = function() {
       $mdDialog.hide();
     };
+
     $scope.cancel = function() {
       $mdDialog.cancel();
     };
-
-
 
 }]);
